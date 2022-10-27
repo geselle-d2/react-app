4f8f84b0b4021adf17411137ab9a2a387216fbc1
p.h <109684733+schlachtergeselle@users.noreply.github.com>
2022-10-27 18:41:39 +0200

    Updates

--- src/components/title.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function(){
-    return(
-        <div className="title main-elements">
-            <h1>Der Schmankerl-Shop</h1>
-        </div>
-    )
-}
